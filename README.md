78580eb7283d7d429f4bf3b18267f783e5fcc273
ilosrim <ilosrim@yahoo.com>
2022-12-14 15:38:43 +0500

    my configs

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 <h3 align="center"><b>ilosrim</b></h3>
-<img src="./.config/win-neofetch.png" alt="Windows PowerShell config" align="center">
+<img src="./ilosrim/powershell.png" alt="Windows PowerShell config" align="center">
 
-* **Operatsion Tizim:** Windows 11 Pro 86_64
-* **Terminal:** Windows Terminal
-* **Shrift:** Hack Regular Nerd Font Complete Mono Windows Compatible
-* **Ranglar Sxemasi:** [`Powerline`](https://github.com/b-ryan/powerline-shell)
+- **Operatsion Tizim:** Windows 10 Pro 86_64
+- **Terminal:** PowerShell Terminal
+- **Shrift:** Raster Fonts
+
+<hr />
+<img src="./ilosrim/PowerShell/win-neofetch.png" alt="Windows PowerShell config" align="center">
+
+- **Operatsion Tizim:** Windows 11 Pro 86_64
+- **Terminal:** Windows Terminal
+- **Shrift:** Hack Regular Nerd Font Complete Mono Windows Compatible
+- **Ranglar Sxemasi:** [`Powerline`](https://github.com/b-ryan/powerline-shell)
+
+<hr />
+<img src="./ilosrim/win-arch-2.png" alt="Windows & Linux" align="center">
+
+- **Operatsion Tizim:** Arch Linux on Windows 10 Pro 86_64
+- **Terminal:** Fish
+- **Shrift:** Hack Regular Nerd Font
+
+<hr />
+<img src="./ilosrim/winMacUx.png" alt="Windows Mac Linux" align="center">
+
+- **Operatsion Tizim:** Ubuntu on Windows 10 Pro 86_64
+- **Terminal:** Fish
+- **Shrift:** Hack Regular Nerd Font
+
+
+<hr />
+<img src="./ilosrim/firefox-theme/images/ff_protonbutquantum.png" alt="Windows PowerShell config" align="center">
+
+- **Operatsion Tizim:** Parqinsa
+
+<h3 align="center">Bu yerda sizning konfiglaringiz bo'lishi mumkin...</h3>
---
+++ ilosrim/PowerShell/powershell_script/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,80 @@
+Clear-Host
+
+# Set Default location
+Set-Location D:
+
+# function prompt { $p = Split-Path -leaf -path (Get-Location) "$p> " }
+
+# function prompt { "[$(('mirjr@dev'+$PWD).replace($HOME, '~'))]> " }
+
+# function prompt { "$( ( Get-Location | Get-Item ).Name )>" }
+
+# function prompt {'mirjr@dev:' + $(Get-Location | Split-Path -Leaf) + "$ "}
+
+function Write-BranchName () {
+	try {
+		$branch = git rev-parse --abbrev-ref HEAD
+
+		if ($branch -eq "HEAD") {
+			# we're probably in detached HEAD state, so print the SHA
+			$branch = git rev-parse --short HEAD
+			Write-Host " ($branch)" -ForegroundColor "red"
+		}
+		else {
+			# we're on an actual branch, so print it
+			Write-Host " ($branch)" -ForegroundColor "blue"
+		}
+	}
+ catch {
+		# we'll end up here if we're in a newly initiated git repo
+		Write-Host " (no branches yet)" -ForegroundColor "yellow"
+	}
+}
+
+function prompt {
+	$base = "mirjr@$env:COMPUTERNAME "
+	$date = (get-date -Format "yyyy-MM-dd HH:mm:ss")
+	$path = "$($executionContext.SessionState.Path.CurrentLocation)" # $(Get-Location | Split-Path -Leaf)
+	$userPrompt = "$('$' * ($nestedPromptLevel + 1)) "
+
+	Write-Host "`n$base" -ForegroundColor DarkGreen -NoNewline
+	write-host "$date" -ForegroundColor Yellow -NoNewline
+	write-host " | " -ForegroundColor DarkGray -NoNewline
+
+	if (Test-Path .git) {
+		Write-Host $path -NoNewline -ForegroundColor Yellow
+		Write-BranchName
+	}
+	else {
+		# we're not in a repo so don't bother displaying branch name/sha
+		Write-Host $path -ForegroundColor Yellow
+	}
+
+	return $userPrompt
+}
+
+
+# Arxiv
+# function Prompt
+# {
+#     write-host "mirjr@$env:COMPUTERNAME " -ForegroundColor DarkGreen -NoNewline
+#     write-host (get-date -Format "yyyy-MM-dd HH:mm:ss") -ForegroundColor Yellow -NoNewline
+#     write-host " | " -ForegroundColor DarkGray -NoNewline
+#     if( (Get-Location).Drive -ne $null)
+#     {
+#         write-host (Get-Location) 
+#         $networkdrive = $false
+#     }
+#     else
+#     {
+#         $networkdrive = $true
+#         $first, $second, $third, $folder = (Get-Location).path.Split("\")
+#         write-host "\" -NoNewline
+#         $folder | foreach-object {
+#             write-host "\$_" -NoNewline
+#         }
+#         write-host
+#     }
+#     write-host "$" -NoNewline
+#     return " "
+# }
